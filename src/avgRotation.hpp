ccb92aed152a6aaca610823b78432ec663f5283b
Kyle Wilson <kwilson24@washcoll.edu>
2017-07-18 15:21:17 -0400

    added relative path for bundlefile. Wrote new rotations averaging system and plugged it in.

--- src/avgRotation.hpp
+++ src/avgRotation.hpp
@@ -4,15 +4,6 @@
 #include <cmath>
 #include <ofMain.h>
 
-class avgRotation {
-public:
 
-    avgRotation();
     
-    void computeAverageRotation(const vector<glm::quat> & rotations, glm::quat * result);
-
-private:
-    //glm::vec3 axis;
-    
-};
-
+void computeAverageRotation(const vector<glm::quat> & rotations, glm::quat * result);
